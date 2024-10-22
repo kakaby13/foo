e1f3c637615a3cb530e9195e414c6243ca684ede
Vlad Gavrilchik <v.gavrilchik@itransition.com>
2024-10-22 22:23:48 +0300

    1

---
+++ Goose/Goose/Program.cs
@@ -0,0 +1,38 @@
+﻿using Goose;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Empty arguments");
+        }
+        else
+        {
+            Console.WriteLine("Arguments: " + string.Join(" ", args));
+            Run(args[0]);
+        }
+    }
+
+    private static void Run(string argument)
+    {
+        var servo = new ServoSg90(RaspberryPiGpioPin.GPIO05);
+        
+        switch (argument)
+        {
+            case "R":
+            case "r":
+                servo.Rotate(To.Right);
+                return;
+            case "L":
+            case "l":
+                servo.Rotate(To.Left);
+                return;
+            case "M":
+            case "m":
+                servo.Rotate(To.Middle);
+                return;
+        }
+    }
+}
---
+++ Goose/Goose/ServoSg90.cs
@@ -0,0 +1,91 @@
+﻿using System.Device.Gpio;
+using System.Diagnostics;
+
+namespace Goose;
+
+public class ServoSg90
+{
+    private readonly GpioController _controller;
+    private readonly GpioPin _motorPin;
+    private readonly ulong _ticksPerMilliSecond = (ulong)(Stopwatch.Frequency) / 1000;
+    
+    public ServoSg90(RaspberryPiGpioPin pin)
+    {
+        try
+        {
+            _controller = new GpioController();
+            _motorPin = _controller.OpenPin(Convert.ToInt32(pin));
+            _motorPin.SetPinMode(PinMode.Output);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("ERROR: GpioInit failed - " + ex);
+        }
+    }
+    
+    public void Rotate(To to)
+    {
+        Rotate(ServoPulseTime(to));
+    }
+
+    private void MillisecondToWait(double millisecondsToWait)
+    {
+        var sw = new Stopwatch();
+        var durationTicks = _ticksPerMilliSecond * millisecondsToWait;
+        sw.Start();
+        while (sw.ElapsedTicks < durationTicks) { }
+    }
+
+    private void Rotate(double motorPulse)
+    {
+        const double totalPulseTime = 50;
+        var timeToWait = totalPulseTime - motorPulse;
+
+        _motorPin.Write(PinValue.High);
+        MillisecondToWait(motorPulse);
+        _motorPin.Write(PinValue.Low);
+        MillisecondToWait(timeToWait);
+        _motorPin.Write(PinValue.Low);
+    }
+
+    private static double ServoPulseTime(To to)
+    {
+        return to switch
+        {
+            To.Left => 2,
+            To.Middle => 1.5,
+            To.Right => 1,
+            _ => -1
+        };
+    }
+}
+
+public enum To {
+    Left = 0,
+    Middle = 1,
+    Right = 2,
+}
+
+public enum RaspberryPiGpioPin
+{
+    GPIO05 = 5,
+    GPIO06 = 6,
+    GPIO07 = 7,
+    GPIO08 = 8,
+    GPIO09 = 9,
+    GPIO10 = 10,
+    GPIO11 = 11,
+    GPIO12 = 12,
+    GPIO13 = 13,
+    GPIO14 = 14,
+    GPIO15 = 15,
+    GPIO16 = 16,
+    GPIO17 = 17,
+    GPIO18 = 18,
+    GPIO19 = 19,
+    GPIO20 = 20,
+    GPIO21 = 21,
+    GPIO22 = 22,
+    GPIO23 = 23,
+    GPIO24 = 24
+}
