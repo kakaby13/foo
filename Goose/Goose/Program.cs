18c6a2b78e43410f1ea47e14444e4c52137abad4
Vlad Gavrilchik <v.gavrilchik@itransition.com>
2024-10-23 10:02:18 +0300

    1

--- Goose/Goose/Program.cs
+++ Goose/Goose/Program.cs
@@ -5,27 +5,19 @@ public class Program
     static void Main(string[] args)
     {
         var servo = new ServoSg90(RaspberryPiGpioPin.GPIO05);
-        // Повернуть сервопривод влево на 5 секунд
-        servo.RotateContinuously(To.Left, 5000);
-
-        // Повернуть сервопривод в середину на 3 секунды
-        servo.RotateContinuously(To.Middle, 3000);
-
-        // Повернуть сервопривод вправо на 2 секунды
+        
+        Console.WriteLine("left");
+        servo.RotateContinuously(To.Left, 2000);
+        Console.WriteLine("Middle");
+        servo.RotateContinuously(To.Middle, 2000);
+        Console.WriteLine("Right");
         servo.RotateContinuously(To.Right, 2000);
     }
 
     private static void Run(string argument)
     {
         var servo = new ServoSg90(RaspberryPiGpioPin.GPIO05);
-                    // Повернуть сервопривод влево на 5 секунд
-            servo.RotateContinuously(To.Left, 5000);
-
-            // Повернуть сервопривод в середину на 3 секунды
-            servo.RotateContinuously(To.Middle, 3000);
-
-            // Повернуть сервопривод вправо на 2 секунды
-            servo.RotateContinuously(To.Right, 2000);
+        
         switch (argument)
         {
             case "R":
