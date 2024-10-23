36caffa3712ba81defea18e57254c56932e66a05
Vlad Gavrilchik <v.gavrilchik@itransition.com>
2024-10-23 10:07:08 +0300

    1

--- Goose/Goose/Program.cs
+++ Goose/Goose/Program.cs
@@ -5,33 +5,41 @@ public class Program
     static void Main(string[] args)
     {
         var servo = new ServoSg90(RaspberryPiGpioPin.GPIO05);
+
+        for (var i = 0; i < 5; i++)
+        {
+            var command = Console.ReadLine();
+            if (!string.IsNullOrEmpty(command))
+            {
+                Run(command, servo);
+            }
+        }
         
-        Console.WriteLine("left");
-        servo.RotateContinuously(To.Left, 2000);
-        Console.WriteLine("Middle");
-        servo.RotateContinuously(To.Middle, 2000);
-        Console.WriteLine("Right");
-        servo.RotateContinuously(To.Right, 2000);
     }
 
-    private static void Run(string argument)
+    private static void Run(string argument, ServoSg90 servo)
     {
-        var servo = new ServoSg90(RaspberryPiGpioPin.GPIO05);
-        
         switch (argument)
         {
             case "R":
+                servo.RotateContinuously(To.Right, 2000);
+                return;
             case "r":
                 servo.Rotate(To.Right);
                 return;
             case "L":
+                servo.RotateContinuously(To.Left, 2000);
+                return;
             case "l":
                 servo.Rotate(To.Left);
                 return;
             case "M":
+                servo.RotateContinuously(To.Middle, 2000);
+                return;
             case "m":
                 servo.Rotate(To.Middle);
                 return;
         }
+        
     }
 }
