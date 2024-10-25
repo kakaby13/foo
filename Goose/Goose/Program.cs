2e70b9b9e78c6eae2ad3b5cb6fd31e60235fcfc8
Vlad Gavrilchik <v.gavrilchik@itransition.com>
2024-10-25 21:02:42 +0300

    1

--- Goose/Goose/Program.cs
+++ Goose/Goose/Program.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Device.Gpio;
-using System.Device.Pwm;
-using System.Threading;
+﻿using System.Device.Pwm;
 
 class Program
 {
     static void Main(string[] args)
     {
-        int servoPin = 12; // Новый пин для подключения сервопривода
+        int servoPin = 18; // Пин для подключения сервопривода
         int frequency = 50; // Частота для SG90 сервопривода
 
-        using (var pwmChannel = PwmChannel.Create(1, servoPin, frequency))
+        using (var pwmChannel = PwmChannel.Create(0, servoPin, frequency))
         {
             pwmChannel.Start();
             while (true)
